d250d713d14afcba5f74a50f7a3595e48b449621
MNGiit <36828920+MNGiit@users.noreply.github.com>
2022-08-26 14:23:53 -0400

    add enemy

--- app.js
+++ app.js
@@ -10,9 +10,28 @@ console.log(countRows());
 console.log(countTiles());
 
 const setBlast = () => {
-    alert("You clicked a tile!");
-    // console.log(this);
+    //  Start blast on origin/click
+    //  for i = 1; i < blastRange; i++
+    //      if tile is next to it on the right, start blast
+    //      if tile is next to it on the left, start blast
+    //      if tile is above it, start blast
+    //      if tile is below it, start blast
 }
+
+const blastCheck = () => {
+    //  if tile has something on it
+    //      remove that something
+    //      add point to score
+}
+
+
+// document.getElementsByClassName("tile").getAttribute("onclick").alert("Test");
+
+let tiles = document.querySelectorAll(".tile");
+console.log(tiles);
+console.log(tiles[0]);
+// tiles[0].style.background = "black";
+tiles[0].innerText = "Enemy";
 
 document.querySelectorAll(".tile").forEach(occurence => {
     occurence.addEventListener('click', (e) => {
@@ -21,7 +40,10 @@ document.querySelectorAll(".tile").forEach(occurence => {
         // console.log(e.path);
         console.log(e.target);
         e.target.style.background = "black";
+        // e.target.nextSibling.style.background = "red";
+        console.log(e.target.nextSibling);
+
+        let nextTile = tiles.indexOf(e.target);
+        console.log(nextTile);
     });
 });
-
-// document.getElementsByClassName("tile").getAttribute("onclick").alert("Test");

64442e2afd3d01d3c978eafbb450a003193ddc8f
MNGiit <36828920+MNGiit@users.noreply.github.com>
2022-08-26 13:47:48 -0400

    create a way to learn what was clicked on

--- app.js
+++ app.js
@@ -11,6 +11,16 @@ console.log(countTiles());
 
 const setBlast = () => {
     alert("You clicked a tile!");
+    // console.log(this);
 }
+
+document.querySelectorAll(".tile").forEach(occurence => {
+    occurence.addEventListener('click', (e) => {
+        console.log("Tile was clicked");
+        console.log(this);
+        console.log(e.path);
+        console.log(e.target);
+    });
+});
 
 // document.getElementsByClassName("tile").getAttribute("onclick").alert("Test");
